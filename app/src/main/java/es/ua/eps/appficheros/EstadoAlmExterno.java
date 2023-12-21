128ef474d1d7d5b1ef54e7995e3025e1062268bb
di-saldana <dsl42@alu.ua.es>
2023-12-21 19:11:19 +0100

    first commit

---
+++ app/src/main/java/es/ua/eps/appficheros/EstadoAlmExterno.java
@@ -0,0 +1,14 @@
+package es.ua.eps.appficheros;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EstadoAlmExterno extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_estado_alm_externo);
+    }
+}
---
+++ app/src/main/java/es/ua/eps/appficheros/MainActivity.java
@@ -0,0 +1,13 @@
+package es.ua.eps.appficheros;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
