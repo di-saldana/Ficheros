e577bf76349ddc87de5d60d725401f8ca06baaee
di-saldana <dsl42@alu.ua.es>
2024-01-08 05:15:49 -0400

    final

--- app/src/main/java/es/ua/eps/appficheros/EstadoAlmExterno.java
+++ app/src/main/java/es/ua/eps/appficheros/EstadoAlmExterno.java
@@ -3,21 +3,45 @@ package es.ua.eps.appficheros;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 import es.ua.eps.appficheros.databinding.ActivityEstadoAlmExternoBinding;
-import es.ua.eps.appficheros.databinding.ActivityFicheroBinding;
 
 public class EstadoAlmExterno extends AppCompatActivity {
+    final String INFO_TYPE = "INFO_TYPE";
+    final String LOCATION_TYPE = "LOCATION_TYPE";
+
+    TextView info;
+    Button volverButton;
+
+    boolean externalStorage = false;
 
     private ActivityEstadoAlmExternoBinding viewBinding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         viewBinding = ActivityEstadoAlmExternoBinding.inflate(getLayoutInflater());
         setContentView(viewBinding.getRoot());
 
+        volverButton = viewBinding.volver;
+        info = viewBinding.textView;
+
+        // Obtener el estado del almacenamiento externo y mostrarlo en el TextView
+        String estado = getText();
+        info.setText(estado);
+
+        // Configurar el botón de volver para regresar a la actividad principal
         viewBinding.volver.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -26,4 +50,98 @@ public class EstadoAlmExterno extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        if(Build.VERSION.SDK_INT >= 30) {
+            if(externalStorage) {
+                externalStorage = false;
+            }
+        }
+    }
+
+    // Obtener información sobre la tarjeta SD externa
+    private String getExternalSDInfo() {
+        StringBuilder info = new StringBuilder();
+
+        info.append("State: ").append(Environment.getExternalStorageState()).append("\n");
+        info.append("Emulated: ").append(Environment.isExternalStorageEmulated()).append("\n");
+        info.append("Removable: ").append(Environment.isExternalStorageRemovable()).append("\n");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            info.append("Legacy: ").append(Environment.isExternalStorageLegacy()).append("\n");
+        }
+
+        info.append("Data Dir: ").append(getDataDir()).append("\n");
+        info.append("Cache Dir: ").append(getApplicationContext().getCacheDir()).append("\n");
+        File sdPath = Environment.getExternalStorageDirectory();
+        info.append("External Storage Dir: ").append(sdPath).append("\n");
+        info.append("External ALARMS Dir: ").append(getApplicationContext().getExternalFilesDir(Environment.DIRECTORY_ALARMS)).append("\n");
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            info.append("External AUDIOBOOKS Dir: ").append(getApplicationContext().getExternalFilesDir(Environment.DIRECTORY_AUDIOBOOKS)).append("\n");
+        }
+        info.append("External DCIM Dir: ").append(getApplicationContext().getExternalFilesDir(Environment.DIRECTORY_DCIM)).append("\n");
+
+        info.append("External DOCUMENTS Dir: ").append(getApplicationContext().getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)).append("\n");
+        info.append("External DOWNLOADS Dir: ").append(getApplicationContext().getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS)).append("\n");
+        info.append("External MOVIES Dir: ").append(getApplicationContext().getExternalFilesDir(Environment.DIRECTORY_MOVIES)).append("\n");
+        info.append("External MUSIC Dir: ").append(getApplicationContext().getExternalFilesDir(Environment.DIRECTORY_MUSIC)).append("\n");
+        info.append("External NOTIFICATIONS Dir: ").append(getApplicationContext().getExternalFilesDir(Environment.DIRECTORY_NOTIFICATIONS)).append("\n");
+        info.append("External PICTURES Dir: ").append(getApplicationContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES)).append("\n");
+        info.append("External PODCASTS Dir: ").append(getApplicationContext().getExternalFilesDir(Environment.DIRECTORY_PODCASTS)).append("\n");
+        info.append("External RINGTONES Dir: ").append(getApplicationContext().getExternalFilesDir(Environment.DIRECTORY_RINGTONES)).append("\n");
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            info.append("External SCREENSHOTS Dir: ").append(getApplicationContext().getExternalFilesDir(Environment.DIRECTORY_SCREENSHOTS)).append("\n");
+        }
+        info.append("Root Dir: ").append(Environment.getRootDirectory()).append("\n");
+
+        return info.toString();
+    }
+
+    // Obtener el texto a mostrar en el TextView según el tipo de información y ubicación seleccionada
+    private String getText() {
+        Intent intent = getIntent();
+        int infoType = intent.getIntExtra(INFO_TYPE, -1);
+        int locationType = intent.getIntExtra(LOCATION_TYPE, -1);
+
+        switch (infoType) {
+            case 0:
+                return getExternalSDInfo();
+            case 1:
+                switch (locationType) {
+                    case 0:
+                        return getFileText(getFilesDir());
+                    case 1:
+                        return getFileText(Environment.getExternalStorageDirectory());
+                    default:
+                        return "Error";
+                }
+            default:
+                return "Error";
+        }
+    }
+
+    // Leer el contenido de un archivo y devolverlo como texto
+    private String getFileText(File dir) {
+        File fileToRead = new File(dir, "information.txt");
+
+        if (fileToRead.exists()) {
+            try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileToRead))) {
+                StringBuilder text = new StringBuilder();
+                String line;
+
+                while ((line = bufferedReader.readLine()) != null) {
+                    text.append(line).append("\n");
+                }
+
+                return text.toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return "El fichero no existe.";
+    }
+
 }
--- app/src/main/java/es/ua/eps/appficheros/MainActivity.java
+++ app/src/main/java/es/ua/eps/appficheros/MainActivity.java
@@ -3,12 +3,36 @@ package es.ua.eps.appficheros;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 import es.ua.eps.appficheros.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    boolean activeSD = false;
+    boolean accessSD = false;
+    private boolean externalStorage = false;
+    private int locationType = 0;
+
+    private EditText textSave;
+    private Button estadoButton;
+    private Button addButton;
+    private Button verButton;
+    private Button externalButton;
+    private Button internalButton;
+    private Button cerrarButton;
 
     private ActivityMainBinding viewBinding;
 
@@ -18,27 +42,182 @@ public class MainActivity extends AppCompatActivity {
         viewBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(viewBinding.getRoot());
 
-        viewBinding.estado.setOnClickListener(new View.OnClickListener() {
+        // Asignación de elementos de la interfaz a variables
+        textSave = viewBinding.textSave;
+        estadoButton = viewBinding.estado;
+        addButton = viewBinding.add;
+        verButton = viewBinding.ver;
+        externalButton = viewBinding.external;
+        internalButton = viewBinding.internal;
+        cerrarButton = viewBinding.cerrar;
+
+        // Deshabilitar el botón interno
+        viewBinding.internal.setEnabled(false);
+
+        // Configuración del botón de estado
+        estadoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Crear una nueva actividad y pasar información adicional
                 Intent intent = new Intent(MainActivity.this, EstadoAlmExterno.class);
+                intent.putExtra("INFO_TYPE", 0);
                 startActivity(intent);
             }
         });
 
-        viewBinding.ver.setOnClickListener(new View.OnClickListener() {
+        // Configuración del botón de agregar
+        addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Fichero.class);
+                // Llamar al método para guardar el texto
+                saveText();
+            }
+        });
+
+        // Configuración del botón de ver
+        verButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Crear una nueva actividad y pasar información adicional
+                Intent intent = new Intent(MainActivity.this, EstadoAlmExterno.class);
+                intent.putExtra("INFO_TYPE", 1);
+                intent.putExtra("LOCATION_TYPE", locationType);
                 startActivity(intent);
             }
         });
 
-        viewBinding.cerrar.setOnClickListener(new View.OnClickListener() {
+        // Configuración del botón externo
+        externalButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Verificar los permisos de acceso y cambiar al almacenamiento externo
+                if (accessPermission()) {
+                    changeToExternal();
+                }
+            }
+        });
+
+        // Configuración del botón interno
+        internalButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Cambiar al almacenamiento interno
+                changeToInternal();
+            }
+        });
+
+        // Configuración del botón cerrar
+        cerrarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Finalizar la actividad
                 finish();
             }
         });
+    }
+
+    // Método onRestart, se ejecuta al reiniciar la actividad
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        // Verificar si se ha cambiado al almacenamiento externo y si se permite el acceso
+        if (Build.VERSION.SDK_INT >= 30) {
+            if (externalStorage) {
+                externalStorage = false;
+                if (Environment.isExternalStorageManager()) {
+                    changeToExternal();
+                }
+            }
+        }
+    }
+
+    // Método para guardar el texto en un archivo
+    private void saveText() {
+        // Obtener el estado del almacenamiento externo
+        String estado = Environment.getExternalStorageState();
+
+        // Verificar si el almacenamiento externo está montado y disponible para escritura
+        if (estado.equals(Environment.MEDIA_MOUNTED)) {
+            activeSD = true;
+            accessSD = true;
+
+            try {
+                File sdPath = null;
+                // Obtener la ruta del almacenamiento interno o externo según el tipo de ubicación seleccionada
+                if (locationType == 0) {
+                    sdPath = getFilesDir();
+                } else {
+                    sdPath = Environment.getExternalStorageDirectory();
+                }
+
+                // Crear un archivo en la ruta especificada y escribir el texto en él
+                File file = new File(sdPath, "information.txt");
+                OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(file));
+                writer.write(textSave.getText().toString());
+                writer.flush();
+                writer.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else if (estado.equals(Environment.MEDIA_MOUNTED_READ_ONLY)) {
+            activeSD = true;
+            accessSD = false;
+        }
+    }
+
+    // Método para mover un archivo de un directorio a otro
+    private void moveFile(File inDir, File outDir) {
+        try {
+            File inFile = new File(inDir, "information.txt");
+            File outFile = new File(outDir, "information.txt");
+
+            FileInputStream inStream = new FileInputStream(inFile);
+            FileOutputStream outStream = new FileOutputStream(outFile);
+
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+
+            while ((bytesRead = inStream.read(buffer)) != -1) {
+                outStream.write(buffer, 0, bytesRead);
+            }
+
+            inStream.close();
+            outStream.close();
+
+            inFile.delete();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Método para cambiar al almacenamiento externo
+    private void changeToExternal() {
+        locationType = 1;
+        internalButton.setEnabled(true);
+        externalButton.setEnabled(false);
+        moveFile(getFilesDir(), Environment.getExternalStorageDirectory());
+    }
+
+    // Método para cambiar al almacenamiento interno
+    private void changeToInternal() {
+        locationType = 0;
+        internalButton.setEnabled(false);
+        externalButton.setEnabled(true);
+        moveFile(Environment.getExternalStorageDirectory(), getFilesDir());
+    }
+
+    // Método para verificar los permisos de acceso al almacenamiento externo
+    private boolean accessPermission() {
+        if (Build.VERSION.SDK_INT >= 30) {
+            if (!Environment.isExternalStorageManager()) {
+                Uri uri = Uri.parse("package:" + BuildConfig.APPLICATION_ID);
+                startActivity(new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION, uri));
+                externalStorage = true;
+                return false;
+            } else {
+                return true;
+            }
+        }
+        return true;
     }
 }
